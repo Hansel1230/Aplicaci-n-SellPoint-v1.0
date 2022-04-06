b139f23d9edf94133e5c806f3fb1c28ae3320183
Jose Daniel Espinal <the_jose_d@hotmail.com>
2022-04-05 22:14:48 -0400

    Cleaning code

--- WindowsFormsAppSellPoint/FormLogin.cs
+++ WindowsFormsAppSellPoint/FormLogin.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsAppSellPoint
@@ -22,11 +15,4 @@ namespace WindowsFormsAppSellPoint
 
         }
     }
-
-
-    //Testing
-
-
-    // hola x2
-//hola 
 }
