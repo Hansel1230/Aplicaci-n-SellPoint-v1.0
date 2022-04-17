f486bb96d094e926e886f9f970b449006648fba0
hansel1230 <hanseldelossantos1230@gmail.com>
2022-04-17 13:22:38 -0400

    Login Realizado, se le debe dar algunos ajustes. El query para validar las credenciales lo realize en la clase login de la capa de presentacion y no en la clase data de la capa de datos 'Se me complico'

--- WindowsFormsAppSellPoint/FormLogin.cs
+++ WindowsFormsAppSellPoint/FormLogin.cs
@@ -1,49 +1,41 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 
 namespace WindowsFormsAppSellPoint
 {
     public partial class FormLogin : Form
     {
+        #region Constructor
         public FormLogin()
         {
             InitializeComponent();
         }
+        #endregion
+        public static bool UserValid { get; set; } = false;
 
+        #region Eventos
         private void FormLogin_Load(object sender, EventArgs e)
         {
             Fultxt();
         }
-
         private void buttonAceptar_Click(object sender, EventArgs e)
         {
-
+            logins();
         }
-
         private void buttonCancelar_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-        #region Metodos
-
-        public void Fultxt()
-        {
-            textBoxUsuario.Text = "Ingrese usuario";
-            textBoxContrasena.Text = "Ingrese Contraseña";
-        }
-
-        #endregion
-        #region Eventos
         private void textBoxUsuario_Click(object sender, EventArgs e)
         {
             if (textBoxUsuario.Text == "Ingrese usuario")
             {
-                 textBoxUsuario.Text = "";
+                textBoxUsuario.Text = "";
             }
         }
-        #endregion
-
         private void textBoxUsuario_Leave(object sender, EventArgs e)
         {
             if (textBoxUsuario.Text == "")
@@ -58,7 +50,6 @@ namespace WindowsFormsAppSellPoint
                 textBoxContrasena.Text = "";
             }
         }
-
         private void textBoxContrasena_Leave(object sender, EventArgs e)
         {
             if (textBoxContrasena.Text == "")
@@ -66,5 +57,46 @@ namespace WindowsFormsAppSellPoint
                 textBoxContrasena.Text = "Ingrese Contraseña";
             }
         }
+        #endregion
+
+        #region Metodos
+        public void Fultxt()
+        {
+            textBoxUsuario.Text = "Ingrese usuario";
+            textBoxContrasena.Text = "Ingrese Contraseña";
+        }
+        public void logins()
+        {
+            try
+            {
+                string DBConnection = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+                using (SqlConnection conexion = new SqlConnection(DBConnection))
+                {
+                    conexion.Open();
+                    using (SqlCommand cmd = new SqlCommand("SELECT UserNameEntidad, PassworEntidad FROM Entidades WHERE UserNameEntidad='" + textBoxUsuario.Text + "' AND PassworEntidad='" + textBoxContrasena.Text + "'", conexion))
+                    {
+                        SqlDataReader dr = cmd.ExecuteReader();
+                        if (dr.Read())
+                        {
+                            MessageBox.Show("Login exitoso.");
+                            UserValid= true;
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Datos incorrectos.");
+                        }
+                        dr.Close();
+                        dr.Dispose();
+                    }
+                    conexion.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        #endregion
     }
 }
